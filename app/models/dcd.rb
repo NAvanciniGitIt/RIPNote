6cb9acffe9fce7107693f0608145a40838617ead
Tasha <navancini@gmail.com>
2021-02-11 14:51:40 -0500

    removed useless

--- app/models/dcd.rb
+++ app/models/dcd.rb
@@ -1,21 +1,16 @@
 class Dcd < ApplicationRecord
-  DCD_RELATIONSHIPS = ["Parent", "Sibling", "Aunt/Uncle", "Cousin", "Niece/Nephew", "Child", "Friend", "Spouse", "Significant Other", "Grandparent", "Other"]
-
-  validate :relationship_valid?
+  
 
   validates :first_name, presence: :true 
   validates :last_name, presence: :true
+  validates :relationship, presence: true
+  validates :birthday, presence: true
+  validates :deathday, presence: true
+  validates :gender, presence: true
 
 
   has_many :users, through: :posts
   has_many :posts, :dependent => :destroy
   has_one_attached :picture
 
-  private
-
-    def relationship_valid?
-      if self.relationship && !DCD_RELATIONSHIPS.include?(self.relationship)
-        self.errors.add(:relationship, "Please pick a valid relationship")
-    end
-  end
 end
