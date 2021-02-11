96c3cad8d7881e775c9b1e8c3afbf7b475d3b6f1
Tasha <navancini@gmail.com>
2021-02-11 14:51:50 -0500

    validations and nesting

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,13 @@
 class Post < ApplicationRecord
-  #accepts_nested_attributes_for :dcds, :users
+  
 
   belongs_to :user
   belongs_to :dcd
   has_many :comments
   has_one_attached :picture
+
+  validates_length_of :post_content, minimum: 1, maximum: 100, allow_blank: false
+  validates :title, presence: true 
+
+  accepts_nested_attributes_for :comments
 end
