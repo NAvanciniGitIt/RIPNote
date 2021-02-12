3f025a5ac1404917c10b60749069f3d1a35958f2
Tasha <navancini@gmail.com>
2021-02-12 00:49:39 -0500

    helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
 
   
+
+  # def find_post(post)
+  #   @post = Post.find_by_id(post) #creating dynamic method works between multiple controllers -- pass in params within this method and does the query for me 
+  # end
+
+  def find_dcd
+    @dcd = Dcd.find_by_id(params[:dcd_id])
+  end
 end
