92cec249c19cc336cce0bc979d607035305cbe4b
Tasha <navancini@gmail.com>
2021-02-12 00:50:16 -0500

    refactor and bug fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
-  
+  before_action :find_dcd, only: [:new, :show]
+  before_action :find_post, only: [:new, :create, :show]
+  before_action :redirect_if_no_post, only: [:show]
+
   def show
-    @post = Post.find(params[:id])
+    @comment = Comment.new
     # binding.pry
   end
 
@@ -12,25 +15,36 @@ class PostsController < ApplicationController
   def new
     # binding.pry
     @post = Post.new 
-    @dcd = Dcd.find(params[:dcd_id])
     render :new_dcd_post
   end
   
   def create
-    @post = Post.new(post_params)
     @dcd = Dcd.find_by(params[:dcd_id])
+    @post = Post.new(post_params)
+    # binding.pry
     if @post.save
-      redirect_to dcds_path
+      # binding.pry
+      redirect_to dcd_post_path(@dcd, @post)
     else
       flash.now[:error] = @post.errors.full_messages
       render :new_dcd_post
     end
   end
-
-
+  
+  
   private 
-
+  
   def post_params
     params.require(:post).permit(:title, :post_content, :user_id, :dcd_id, :picture)
   end
+  
+  def redirect_if_no_post
+    # binding.pry
+    redirect_to dcd_posts_path unless @post
+  end 
+
+  def find_post
+    @post = Post.find_by_id(params[:id])
+  end 
+
 end
