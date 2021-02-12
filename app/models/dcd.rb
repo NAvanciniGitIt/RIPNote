44fe971ab80aabac690f2f705cb349b9e2c74273
Tasha <navancini@gmail.com>
2021-02-12 00:50:30 -0500

    new query

--- app/models/dcd.rb
+++ app/models/dcd.rb
@@ -9,8 +9,27 @@ class Dcd < ApplicationRecord
   validates :gender, presence: true
 
 
-  has_many :users, through: :posts
-  has_many :posts, :dependent => :destroy
+  belongs_to :user
+  has_many :posts, dependent: :delete_all
   has_one_attached :picture
 
+
+  # @search = params["search"]
+  #   if @search.present?
+  #     @first_name = @search["first_name"]
+  #     @dcds = Dcd.where("first_name LIKE ?", "%#{@first_name}%")
+  #   end
+
+  # def self.search_dcd(name)
+  #   where("first_name, last_name LIKE ?", "%#{name}%")
+  # end 
+
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def self.search(params)
+    where("LOWER(first_name) || ' ' || LOWER(last_name) LIKE :s", :s => "%#{params.downcase}%")
+  end #downcase attributes ,, checking both i guess, stage parameter assigning to key of S our params downcased -- looking for similarities. 
 end
